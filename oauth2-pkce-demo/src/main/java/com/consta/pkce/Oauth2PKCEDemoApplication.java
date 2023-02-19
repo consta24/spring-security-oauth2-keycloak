ae9f59d5eadf7a5f475e54e10153217139a1f42d
consta24 <andrei24constantin@gmail.com>
2023-02-19 17:09:16 +0200

    init

---
+++ oauth2-pkce-demo/src/main/java/com/consta/pkce/Oauth2PKCEDemoApplication.java
@@ -0,0 +1,11 @@
+package com.consta.pkce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2PKCEDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2PKCEDemoApplication.class, args);
+    }
+}
---
+++ oauth2-pkce-demo/src/main/java/com/consta/pkce/controller/HomeRestController.java
@@ -0,0 +1,15 @@
+package com.consta.pkce.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/home")
+@CrossOrigin(origins = "*")
+public class HomeRestController {
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String home() {
+        return "Hello";
+    }
+}
